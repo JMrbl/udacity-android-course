f0cf6ac1a13d4b7eb89134adc03adb9615659d82
jsmirabal <jmirabal15k@gmail.com>
2017-08-06 20:54:34 -0500

    Implement limit on preferences part 2

--- app/src/main/java/japps/sunshine_version_julio/views/LocationEditTextPreference.java
+++ app/src/main/java/japps/sunshine_version_julio/views/LocationEditTextPreference.java
@@ -1,12 +1,19 @@
 package japps.sunshine_version_julio.views;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.support.annotation.RequiresApi;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import japps.sunshine_version_julio.R;
 
@@ -16,6 +23,7 @@ import japps.sunshine_version_julio.R;
 
 public class LocationEditTextPreference extends EditTextPreference {
     private static final String LOG_TAG = LocationEditTextPreference.class.getSimpleName();
+    private static final int MIN_LENGHT = 3;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public LocationEditTextPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
@@ -42,5 +50,47 @@ public class LocationEditTextPreference extends EditTextPreference {
                 R.styleable.LocationEditTextPreference,
                 0, 0);
         Log.d(LOG_TAG, String.valueOf(typedArray.getInteger(R.styleable.LocationEditTextPreference_minLength, 0)));
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        EditText editText = getEditText();
+        editText.addTextChangedListener(onPreferenceChangeListener());
+    }
+
+    private TextWatcher onPreferenceChangeListener() {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                handleDialog(getDialog(), editable);
+            }
+        };
+    }
+
+    private void handleDialog(Dialog dialog, Editable editable) {
+        if (dialog instanceof AlertDialog){
+            AlertDialog alertDialog = (AlertDialog) dialog;
+            Button positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+            handlePositiveButtonAvailability(positiveButton, editable);
+        }
+    }
+
+    private void handlePositiveButtonAvailability(Button positiveButton, Editable editable) {
+        if (editable.length() < MIN_LENGHT){
+            positiveButton.setEnabled(false);
+        } else {
+            positiveButton.setEnabled(true);
+        }
     }
 }
